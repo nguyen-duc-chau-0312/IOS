5fc10ff7ad20fb32e05d73b9275574bf08df0768
nguyen-duc-chau-0312 <nguyenducchau.1997@gmail.com>
2022-05-09 16:06:45 +0700

    Delegate

---
+++ DelegatePattern/DelegatePattern/CustomView/UserView.swift
@@ -0,0 +1,20 @@
+//
+//  UserView.swift
+//  DelegatePattern
+//
+//  Created by Nguyen Duc Chau on 09/05/2022.
+//
+
+import UIKit
+
+class UserView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DelegatePattern/DelegatePattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegatePattern
+//
+//  Created by Nguyen Duc Chau on 09/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
